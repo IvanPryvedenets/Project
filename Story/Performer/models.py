2889388cc405ef13a55843ae802e1b1ec3cb6acf
Ivan Pryvedenets <pryvivan@gmail.com>
2019-06-11 01:26:08 +0300

    Start project: 2 models

---
+++ Story/Performer/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Category(models.Model):
+    field = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Category: {}'.format(self.field)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField()
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    old_price = models.DecimalField(max_digits=3, decimal_places=2)
+    price = models.DecimalField(max_digits=3, decimal_places=2)
+    slug = models.CharField(max_length=100)
+    stock = models.CharField(max_length=25, default='В наявності')
+
+    def get_absolute_url(self):
+        return 
